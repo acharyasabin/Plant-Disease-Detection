b17d5b27c5daf1ba0deea00a9657e77d0cb1e394
acharyasabin <acharyasabin1997@gmail.com>
2019-11-26 14:58:13 +0545

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Plant-Disease-Detection
+
+
+# References:
+1. https://github.com/googlecodelabs/tensorflow-for-poets-2
---
+++ predict.py
@@ -0,0 +1,102 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+
+def load_graph(model_file):
+  graph = tf.Graph()
+  graph_def = tf.GraphDef()
+
+  with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+  with graph.as_default():
+    tf.import_graph_def(graph_def)
+
+  return graph
+
+def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
+				input_mean=0, input_std=255):
+  input_name = "file_reader"
+  output_name = "normalized"
+  img = cv2.imread(file_name)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+  float_caster = tf.cast(img, tf.float32)
+
+  dims_expander = tf.expand_dims(float_caster, 0);
+
+  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+
+  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+
+  sess = tf.Session()
+  result = sess.run(normalized)
+
+  return result
+
+def load_labels(label_file):
+  label = []
+  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  for l in proto_as_ascii_lines:
+    label.append(l.rstrip())
+  return label
+
+if __name__ == "__main__":
+  file_name = "test1.JPG"
+  model_file = "files/retrained_graph.pb"
+  label_file = "files/retrained_labels.txt"
+  input_height = 224
+  input_width = 224
+  input_mean = 128
+  input_std = 128
+  input_layer = "input"
+  output_layer = "final_result"
+
+  graph = load_graph(model_file)
+  t = read_tensor_from_image_file(file_name,
+                                  input_height=input_height,
+                                  input_width=input_width,
+                                  input_mean=input_mean,
+                                  input_std=input_std)
+
+ 
+  input_name = "import/" + input_layer
+  
+  output_name = "import/" + output_layer
+  input_operation = graph.get_operation_by_name(input_name);
+  output_operation = graph.get_operation_by_name(output_name);
+
+  with tf.Session(graph=graph) as sess:
+    start = time.time()
+    results = sess.run(output_operation.outputs[0],
+                      {input_operation.outputs[0]: t})
+    end=time.time()
+  results = np.squeeze(results)
+  pr = results.argmax()
+  print(pr)
+
+
+  labels = load_labels(label_file)
+  print("Prediction",labels[pr])
+
